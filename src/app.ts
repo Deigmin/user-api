4afde104453dcecf0a6d5361b5b5992d8c8b632a
3DPDB1D9G <vpldzzwaac2i@mail.ru>
2021-03-13 18:34:19 +0400

    add session

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,29 @@
 import express, { Express } from "express";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
+import session from "express-session";
 
 import authRouter from "./routes/auth";
 import usersRouter from "./routes/users";
 
 let app: Express = express();
 
+declare module "express-session" {
+    interface Session {
+        token: string
+    }
+}
+
+app.use(session({
+    secret: "secret key",
+    cookie: { maxAge: 60000 },
+    resave: false,
+    saveUninitialized: false
+}));
+
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 
 app.use("/api-token-auth", authRouter);
