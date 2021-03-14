62e6e243c9f4c92c2fefa22c61bdc4ca20c1d815
3DPDB1D9G <vpldzzwaac2i@mail.ru>
2021-03-14 14:44:39 +0400

    add update last login field

--- src/models/auth.ts
+++ src/models/auth.ts
@@ -1,10 +1,9 @@
 import { User, IUser } from "../db/connection";
 import crypto from "crypto";
-import * as jwt from "jsonwebtoken";
+import { JWT as jwt } from "./JWT";
 
 export class AuthService {
-    constructor() {
-    }
+    private constructor() {}
 
     public static async login(username: string, password: string): Promise<any> {
         let user: User | null = await User.findOne({
@@ -23,43 +22,17 @@ export class AuthService {
             throw new Error("Incorrect password");
         }
 
+        await User.update({ lastLogin: new Date() }, {
+           where: {
+               id: user.id
+           }
+        });
+
         return {
             user: {
                 name: user.username
             },
-            token: this.generateJWT(user)
+            token: jwt.generateJWT(user)
         }
-    }
-
-    public static async signUp(userProperty: IUser): Promise<any> {
-        let salt: string = crypto.randomBytes(32).toString("hex");
-        let hashedPassword: string = crypto.pbkdf2Sync(userProperty.password, salt, 1000, 64, "sha512").toString("hex");
-        let user: User = await User.create({
-            username: userProperty.username,
-            salt: salt,
-            password: hashedPassword,
-            firstName: userProperty.firstName,
-            lastName: userProperty.lastName,
-            isActive: true,
-            lastLogin: new Date(),
-            isSuperuser: false
-        });
-
-        const token: string = this.generateJWT(user);
-        return {
-            user: {
-                username: user.username
-            },
-            token
-        };
-    }
-
-    private static generateJWT(user: User): string {
-        return jwt.sign({
-            data: {
-                id: user.id,
-                username: user.username,
-            }
-        }, "Sup3R_sEcrEt_Key3", {expiresIn: "6h", algorithm: "HS512"});
     }
 }
